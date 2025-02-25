8f3ef7de02be198904a86eab2e258efcc1ef74c1
GersonBlodia <gerson.vera@upsjb.edu.pe>
2025-02-25 15:20:25 -0500

    cambio realizado diseño e iconos

--- src/Components/home/CursoContainer.tsx
+++ src/Components/home/CursoContainer.tsx
@@ -6,7 +6,7 @@ import { WelcomeSection } from "./WelcomeSection";
 
 export const CursoContainer = () => {
   return (
-    <ContextCurso className={" mx-auto flex flex-col md:flex-row justify-between p-8 w-full xl:max-w-6xl  "}>
+    <ContextCurso className={" mx-auto flex flex-col md:flex-row justify-between p-8 w-full md:w-[80%]  "}>
       <div className="w-full md:w-1/2 pr-8">
         <WelcomeSection />
         <ConsentOptions />
