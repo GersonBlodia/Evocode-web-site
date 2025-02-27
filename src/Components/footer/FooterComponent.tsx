48836f6e8b5160e929fd7147dbb0dd39ad81b500
Valentino <valentinoof.dev@gmail.com>
2025-02-27 12:04:55 -0500

    feat: Se agrego el HeroSectionComponent y se configuro un bg en tailwind.config.ts, seguimos usando Lucide React para los icons

--- src/Components/footer/FooterComponent.tsx
+++ src/Components/footer/FooterComponent.tsx
@@ -46,14 +46,14 @@ export const FooterComponent = () => {
         </div>
 
         <div className="footer-section">
-        <h3 className="footer-title">Cursos y Asesoría</h3>
+        <h3 className="footer-title">Galería</h3>
         <div className="gallery">
-            <img src="/images/angularjs.png" alt="Gallery 1" />
-            <img src="/images/React.png" alt="Gallery 2" />
-            <img src="/images/GitHub.png" alt="Gallery 3" />
-            <img src="/images/java.png" alt="Gallery 4" />
-            <img src="/images/sql.png" alt="Gallery 5" />
-            <img src="images/android.png" alt="Gallery 6" />
+            <img src="/images/gallery1.jpg" alt="Gallery 1" />
+            <img src="/images/gallery2.jpg" alt="Gallery 2" />
+            <img src="/images/gallery3.jpg" alt="Gallery 3" />
+            <img src="/images/gallery4.jpg" alt="Gallery 4" />
+            <img src="/images/gallery5.jpg" alt="Gallery 5" />
+            <img src="/images/gallery6.jpg" alt="Gallery 6" />
         </div>
         </div>
         <div className="footer-bottom">
--- src/app/(Evocode)/page.tsx
+++ src/app/(Evocode)/page.tsx
@@ -1,4 +1,5 @@
 import { MainHeader, MainHome } from "@/Components"
+import ITServiceSection from "@/Components/about-company/HeroSectionComponent"
 import { CursoContainer } from "@/Components/home/CursoContainer"
 import PricingSection from "@/Components/pricing-card/PlansComponent"
 
@@ -8,6 +9,7 @@ const PageHome = () => {
     <main className="relative">
         <MainHeader/>
   <MainHome  className="w-[100%]  main-home  mx-auto flex flex-col gap-16">
+          <ITServiceSection/>
           <CursoContainer/>
           <PricingSection/>
   </MainHome>
