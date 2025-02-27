0fac82fd4380a32de76309c587ab0f1ccb6faeef
Valentino <valentinoof.dev@gmail.com>
2025-02-27 00:27:05 -0500

    feat: Se agrego el PlansComponent para las cards de precios con diseño responsivo

--- src/app/(Evocode)/page.tsx
+++ src/app/(Evocode)/page.tsx
@@ -1,5 +1,6 @@
 import { MainHeader, MainHome } from "@/Components"
 import { CursoContainer } from "@/Components/home/CursoContainer"
+import PricingSection from "@/Components/pricing-card/PlansComponent"
 
 
 const PageHome = () => {
@@ -8,6 +9,7 @@ const PageHome = () => {
         <MainHeader/>
   <MainHome  className="w-[100%]  main-home  mx-auto flex flex-col gap-16">
           <CursoContainer/>
+          <PricingSection/>
   </MainHome>
   </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'planes': "url('/images/bg-white.jpg')",
+      }
     },
   },
   plugins: [],
